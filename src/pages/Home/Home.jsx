85754b9712239abbc305a9ba437807e1e1d37fbd
Lucy20209060 <550502661@qq.com>
2018-07-16 23:12:29 +0800

    docs:ice开源版没有ajax功能组件

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { http } from '../../utils/http';
+// import { http } from '../../utils/http';
 
 export default class Home extends Component {
   static displayName = 'Home';
@@ -14,14 +14,14 @@ export default class Home extends Component {
   }
 
   getList = async () => {
-    http('/rpa/data/homepage/app/develop/data',parmas).then(res=>{
-      console.log(1,res) // 2
-    });
-    console.log(2) // 1
+    // http('/rpa/data/homepage/app/develop/data',parmas).then(res=>{
+    //   console.log(1,res) // 2
+    // });
+    // console.log(2) // 1
     
-    let tem = await http('/rpa/data/homepage/app/develop/data',parmas);
-    console.log(3,tem) // 3
-    console.log(4) // 4
+    // let tem = await http('/rpa/data/homepage/app/develop/data',parmas);
+    // console.log(3,tem) // 3
+    // console.log(4) // 4
   }
 
   render() {
